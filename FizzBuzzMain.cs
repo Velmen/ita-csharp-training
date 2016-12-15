686c4c41928961fb8ff44c63b6cf394d56c3f8f9
camdavis <velmen.c@gmail.com>
2016-12-15 11:33:14 -0500

    Added FIzzBuzz

--- FizzBuzzMain.cs
+++ FizzBuzzMain.cs
@@ -16,10 +16,37 @@ namespace ita_chsharp_training
            
 
         }
-        public string ExecuteFizzBuzz()
+        public string ExecuteFizzBuzz(int x)
         {
+            var y = x % 3;
+            var z = x % 5;
 
-            return string.Empty;
+            if ((y == 0 && z == 0) == true)
+            {
+                Console.WriteLine("FizzBuzz!");
+                
+                return Convert.ToString(x);
+
+            }
+
+            else if (y == 0)
+            {
+                Console.WriteLine("Fizz!");
+                return Convert.ToString(x);
+
+            }
+            //////
+            else if (z == 0)
+            {
+                Console.WriteLine("Buzz");
+                return Convert.ToString(x);
+
+            }
+            //////
+
+            else
+                return Convert.ToString(x);
+            Console.ReadKey();
         }
     }
 }
